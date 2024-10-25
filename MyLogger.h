2b8aa1884437e09ccc84aa8500ef66ed357d84e5
Cahú Victor <33499532+CahuVictor@users.noreply.github.com>
2024-10-25 08:51:48 -0300

    Atualização de texto e formatação
    Atualização de texto e formatação

--- MyLogger.h
+++ MyLogger.h
@@ -9,16 +9,16 @@
 	#define __ERROR   1
 
 	#define LOG_DEBUG(interface, message)   MyLogger::logMessageFunc(interface, __DEBUG, message, __FUNCTION__, __LINE__)
-  #define LOG_INFO(interface, message)    MyLogger::logMessageFunc(interface, __INFO, message, __FUNCTION__, __LINE__)
-  #define LOG_WARNING(interface, message) MyLogger::logMessageFunc(interface, __WARNING, message, __FUNCTION__, __LINE__)
-  #define LOG_ERROR(interface, message)   MyLogger::logMessageFunc(interface, __ERROR, message, __FUNCTION__, __LINE__)
+    #define LOG_INFO(interface, message)    MyLogger::logMessageFunc(interface, __INFO, message, __FUNCTION__, __LINE__)
+    #define LOG_WARNING(interface, message) MyLogger::logMessageFunc(interface, __WARNING, message, __FUNCTION__, __LINE__)
+    #define LOG_ERROR(interface, message)   MyLogger::logMessageFunc(interface, __ERROR, message, __FUNCTION__, __LINE__)
 
-  class MyLogger {
-    public:
-      static void logMessageFunc(Print* interface, int level, const char* message, const char* func, int line);
-  
-    private:
-      static void logToInterface(Print* interface, const char* log);
-  };
+    class MyLogger {
+        public:
+            static void logMessageFunc(Print* interface, int level, const char* message, const char* func, int line);
+
+        private:
+            static void logToInterface(Print* interface, const char* log);
+    };
 
 #endif
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Lib-MyLogger
  Repositorio da biblioteca MyLogger para IDE Arduino
+
+Funcionalidades da Biblioteca:
+    Parâmetros de logs, o objetivo é acelerar o desenvolvimento e correção de bugs:
+        DEBUG - Setando este parâmetro nos logs de desenvolvimento, você pode ocultar após o desenvolvimento sem precisar atualizar o código
+	    INFO  - logs necessários do sistema em produção
+	    WARNING - logs de Warning de algum parâmetro acompanhado, os warnings não impedem uma tarefa
+	    ERROR   - logs de erro;
+
+    Para o log terá que ser passado as seguintes informações:
+        Interface - Essa biblioteca foi construída para funcionar na interface serial ou em outras interfaces, sendo necessário informar qual é a interface utilizada.
+        Parâmetro de Debug - Explicado acima
+        Mensagem
+    
+    O log registrará as seguintes informações:
+        Parâmetro de Debug - Explicado acima
+        Mensagem
+        *Funcion - Informará qual é a função que o log está sendo executado
+        *Linha - Informará qual é a linha que o log for executado
+
+Ao identificar alguma anormalidade, com o log é possível saber qual linha e qual função foi registrado a anormalidade facilitando a identificação do problema.
